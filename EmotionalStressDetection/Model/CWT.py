8e1a9115cc2190635b5ab7a026a016bfeeac9730
Thanh-2004 <thanh.20040803@gmail.com>
2024-05-31 19:30:10 +0700

    add CWT

--- EmotionalStressDetection/Model/CWT.py
+++ EmotionalStressDetection/Model/CWT.py
@@ -11,12 +11,12 @@ from scipy.interpolate import interp1d
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.callbacks import EarlyStopping
+import pickle
 np.random.seed(1)
 
 # Datas
-files = {("BachBuon.txt", -1), ("BachCalm.txt", 0), ("QuangCalm3.txt", 0), ("QuangVui3.txt", 1), ("SonBuon3.txt", -1), ("SonBuon4.txt", -1), ("SonVui4.txt", 1), ("ThaiBuon.txt", -1), ("ThaiCalm.txt", 0), ("ThaiCalm2.txt", 0), ("ThaiVui.txt", 1), ("ThaiVui2.txt", 1), ("ThanhfBuon.txt", -1), ("ThanhfCalm.txt", 0), ("ThanhfCalm2.txt", 0), ("ThanhfVui.txt", 1), ("ThanhfVui2.txt", 1)}
+files = {("BachBuon.txt", 0), ("BachCalm.txt", 1), ("QuangCalm3.txt", 1), ("QuangVui3.txt", 2), ("SonBuon3.txt", 0), ("SonBuon4.txt", 0), ("SonVui4.txt", 2), ("ThaiBuon.txt", 0), ("ThaiCalm.txt", 1), ("ThaiCalm2.txt", 1), ("ThaiVui.txt", 2), ("ThaiVui2.txt", 2), ("ThanhfBuon.txt", 0), ("ThanhfCalm.txt", 1), ("ThanhfCalm2.txt", 1), ("ThanhfVui.txt", 2), ("ThanhfVui2.txt", 2)}
 
-# files = {("BachBuon.txt", -1), ("BachCalm.txt", 0), ("QuangVui3.txt", 1)}
 
 # Preprocessing
 def file_signals(processed_file: list, window_size: int):
@@ -40,7 +40,7 @@ def filter(data):
 datas, classes = [], []
 
 for filename, label in files:
-    raw_data = np.loadtxt("CollectedData/new_data/" + filename)
+    raw_data = np.loadtxt("../CollectedData/new_data/" + filename)
     filtered_data = filter(raw_data)
     classes.append(label)
     datas.append(filtered_data)
@@ -64,7 +64,7 @@ signals = np.array(signals)[permutation]
 labels = np.array(labels)[permutation]
 
 # Time-frequency domain extractions using Continuous Wavelet Transform
-def tfDomain(signals, scales = range(1, 256), waveletName = 'morl'):
+def tfDomain(signals, scales = range(1, 128), waveletName = 'morl'):
     cwt_data = np.ndarray(shape = (len(signals), len(scales), len(scales)))
     for i in range(len(signals)):
         coef, freq = pywt.cwt(signals[i], scales, wavelet = waveletName)
@@ -79,13 +79,14 @@ def statistics(model, x_test, y_test):
     y_pred = model.predict(x_test)
     y_pred = np.argmax(y_pred, axis = 1)
     y_test = np.argmax(y_test, axis = 1)
-    print(classification_report(y_test, y_pred, labels = [-1, 0, 1]))
-    # disp = ConfusionMatrixDisplay(confusion_matrix(y_test, y_pred), display_labels = ["Sad", "Calm", "Happy"])
-    # disp.plot()
-    # plt.show()
+    print(classification_report(y_test, y_pred, labels = [0, 1, 2]))
+    disp = ConfusionMatrixDisplay(confusion_matrix(y_test, y_pred), display_labels = ["Stressed", "Calm", "Happy"])
+    disp.plot()
+    plt.show()
     return
 
 def performance(history):
+    ''' Plot the model's accuracy and loss on the training and validation datasets. '''
     plt.figure(figsize=(12, 4))
 
     plt.subplot(1, 2, 1)
@@ -115,7 +116,6 @@ x_train, x_test, y_train, y_test = train_test_split(
     shuffle = True, stratify = labels, random_state=1
 )
 
-# Model
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 
@@ -123,12 +123,13 @@ y_train = keras.utils.to_categorical(y_train, 3)
 y_test = keras.utils.to_categorical(y_test, 3)
 
 model = Sequential(
-    [Conv1D(32, kernel_size = 2, activation = 'relu', input_shape = (255, 255)),
+    [
+     Conv1D(32, kernel_size = 2, activation = 'relu', input_shape = (127, 127)),
      MaxPooling1D(pool_size = 2), 
-     Conv1D(64, kernel_size = 5, activation = 'relu'), 
+     Dropout(0.5),
+     Conv1D(64, kernel_size = 2, activation = 'relu'), 
      MaxPooling1D(pool_size = 2),
-     Conv1D(128, kernel_size = 2, activation = 'relu'), 
-     MaxPooling1D(pool_size = 2),
+     Dropout(0.5)
      ]
 )
 
@@ -140,11 +141,22 @@ model.compile(loss = keras.losses.categorical_crossentropy,
               optimizer = keras.optimizers.Adam(learning_rate= 0.001), 
               metrics = ['acc'])
 
-early_stopping = EarlyStopping(monitor = 'val_loss', patience= 10, restore_best_weights= True)
+early_stopping = EarlyStopping(monitor = 'val_loss', patience= 20, restore_best_weights= True)
 
 history = model.fit(x_train, y_train, batch_size = 32, epochs = 100, verbose = 1, validation_data = (x_test, y_test), callbacks = [early_stopping])
 
 statistics(model, x_test, y_test)
-performance(history)
 
-model.save("a5secs.h5")
+train_loss, train_accuracy = model.evaluate(x_train, y_train, verbose = 0)
+test_loss, test_accuracy = model.evaluate(x_test, y_test, verbose=0)
+
+print(f'Training Accuracy: {train_accuracy}')
+print(f'Test Accuracy: {test_accuracy}')
+
+performance(history)
+# model.save("CNN.h5")
+
+filename = f'../trained_model/CWT.h5'
+pickle.dump(model, open(filename, 'wb'))
+with open('../trained_model/CWTscaler.pkl', 'wb') as file:
+    pickle.dump(scaler, file)
